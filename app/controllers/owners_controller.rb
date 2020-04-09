8533bfec592acd8c1ed7d312b6a60c09850e2bb3
Russell Macalintal <russell.macalintal@gmail.com>
2020-04-09 05:39:07 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -12,6 +12,8 @@ class OwnersController < ApplicationController
 
   post '/owners' do
     @owner = Owner.create(params[:owner])
+    if !params[:pet[:name]].empty?
+      @owner.pets << Pet.create(name: params[:pet][:name])
     redirect "/owners/#{@owner.id}"
   end
 
