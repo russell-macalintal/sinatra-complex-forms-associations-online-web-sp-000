49ba3dacb1134bcf73c90e6cbf6325179875b009
Russell Macalintal <russell.macalintal@gmail.com>
2020-04-09 06:45:03 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -30,10 +30,6 @@ class OwnersController < ApplicationController
   end
 
   patch '/owners/:id' do
-    if !params[:owner].keys.include?("pet_ids")
-    params[:owner]["pet_ids"] = []
-    end
-
     @owner = Owner.find(params[:id])
     @owner.update(params[:owner])
     if !params[:pet][:name].empty?
