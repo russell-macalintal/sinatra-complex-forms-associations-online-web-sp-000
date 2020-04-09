d1bb8a49e434fe73861bc76b76ba418f5bd2257e
Russell Macalintal <russell.macalintal@gmail.com>
2020-04-09 06:52:11 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -21,7 +21,7 @@ class OwnersController < ApplicationController
   get '/owners/:id/edit' do
     @owner = Owner.find(params[:id])
     # @pets = Pet.all
-    erb :'/owners/index'
+    erb :'/owners/edit'
   end
 
   get '/owners/:id' do
